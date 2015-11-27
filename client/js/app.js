8aa966890f1c23aba9f79bcb6eccbc4d3b427336
dkoleary88 <dan@dko.io>
2015-11-26 20:24:24 -0800

    Clean up for basic functionality

--- client/js/app.js
+++ client/js/app.js
@@ -10,19 +10,38 @@ angular.module('SourceApp', [])
   }])
 
   .controller('SourceCtrl', ['$scope', 'SourceFactory', function($scope, SourceFactory) {
-    $scope.url = 'http://';
     $scope.html = '';
     $scope.tags = {};
 
+    $scope.toggleButton = function (enable) {
+      if (enable) {
+        $scope.buttonText = 'Get Source Code';
+        $scope.loading = false;
+      } else {
+        $scope.buttonText = 'Loading...';
+        $scope.loading = true;
+      }
+    };
+
     $scope.resetURL = function () {
       $scope.url = 'http://';
+      $scope.invalidURL = false;
     };
 
     $scope.source = function() {
+      $scope.toggleButton(false);
       SourceFactory.getSource($scope.url)
         .then(function(res) {
           $scope.html = res.data.html;
           $scope.tags = res.data.tags;
+          $scope.toggleButton(true);
+        })
+        .catch(function (err) {
+          $scope.invalidURL = true;
+          $scope.toggleButton(true);
         });
     };
+
+    $scope.toggleButton(true);
+
   }]);
